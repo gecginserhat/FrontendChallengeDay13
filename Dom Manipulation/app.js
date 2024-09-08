0925aacc1f7538855c7aae49e31802ea75beca11
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-08 22:49:28 +0300

    Add files via upload

---
+++ Dom Manipulation/app.js
@@ -0,0 +1,36 @@
+// Window Object
+
+let value;
+
+value = document;
+value = document.all;
+value = document.all.length;
+value = document.all[6];
+
+const elements = document.all;
+
+// for (let i = 0; i < elements.length; i++) {
+//     console.log(elements[i]);
+// }
+
+// elements.forEach(function(element) {
+//     console.log(element);
+// }
+
+/*
+const collection = Array.from(document.all);
+
+
+collections.forEach(function(collection) {
+    console.log(collection);
+});*/
+
+value = document.all[8]
+value = document.body;
+value = document.head;
+value = document.location.hostname;
+value = document.location.port;
+
+value = document.URL;
+
+ console.log(value);
